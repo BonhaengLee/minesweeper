dea2575457a66fb79d8193423cf6554c9ea7e8d8
BonhaengLee <bon952014@ajou.ac.kr>
2022-02-16 02:20:09 +0900

    typescript interface and generic

---
+++ src/test.js
@@ -0,0 +1,9 @@
+// type Permissions = "admin" | "user" | "manager";
+var user = {
+    name: "John",
+    surname: "Doe",
+    age: 30,
+    isAdmin: true,
+    account: 100,
+    permissions: ["admin", "user", "manager"]
+};
