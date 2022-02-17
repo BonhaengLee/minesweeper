f396e26bd7ec80d8bfccd2a45671e9f581de6414
BonhaengLee <bon952014@ajou.ac.kr>
2022-02-18 03:04:27 +0900

    TDD and TLD

--- src/test.js
+++
@@ -1,9 +0,0 @@
-// type Permissions = "admin" | "user" | "manager";
-var user = {
-    name: "John",
-    surname: "Doe",
-    age: 30,
-    isAdmin: true,
-    account: 100,
-    permissions: ["admin", "user", "manager"]
-};
