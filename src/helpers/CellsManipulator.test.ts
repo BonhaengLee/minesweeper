14abb84262fa9d47356af2da699744415d182543
BonhaengLee <bon952014@ajou.ac.kr>
2022-02-20 04:05:24 +0900

    Feat: Field generator part4 2

--- src/helpers/CellsManipulator.test.ts
+++ src/helpers/CellsManipulator.test.ts
@@ -1,26 +1,31 @@
-import { incrementNeighbours } from "./CellsManipulator";
+import { getNeighboursItems, incrementNeighbours } from "./CellsManipulator";
 import { CellState } from "./Field";
 
 const { empty, bomb } = CellState;
 
 describe("Check neighbours selectors", () => {
-  describe("Simple cases", () => {
-    it("Field with only one item", () => {
-      expect(incrementNeighbours([0, 0], [[bomb]])).toStrictEqual([[bomb]]);
+  it("With [0, 0] coords", () => {
+    expect(getNeighboursItems([0, 0])).toStrictEqual({
+      top: [-1, 0],
+      topRight: [-1, 1],
+      right: [0, 1],
+      rightBottom: [1, 1],
+      bottom: [1, 0],
+      bottomLeft: [1, -1],
+      left: [0, -1],
+      leftTop: [-1, -1],
     });
-    it("Field 2x2 with one mine", () => {
-      expect(
-        incrementNeighbours(
-          [0, 0],
-          [
-            [bomb, empty],
-            [empty, empty],
-          ]
-        )
-      ).toStrictEqual([
-        [bomb, 1],
-        [1, 1],
-      ]);
+  });
+  it("With [3, 3] coords", () => {
+    expect(getNeighboursItems([3, 3])).toStrictEqual({
+      top: [2, 3],
+      topRight: [2.4],
+      right: [3, 4],
+      rightBottom: [4, 4],
+      bottom: [4, 3],
+      bottomLeft: [4, 2],
+      left: [3, 2],
+      leftTop: [2, 2],
     });
   });
 });
--- src/helpers/CellsManipulator.ts
+++ src/helpers/CellsManipulator.ts
@@ -1,5 +1,19 @@
-import { Cell, Coord, Field } from "./Field";
+import { Cell, Coords, Field } from "./Field";
 
-export const incrementNeighbours = (coord: Coord, field: Field): Field => {
+export const getNeighboursItems = ([y, x]: Coords): Record<
+  string,
+  [number, number]
+> => ({
+  top: [y - 1, x],
+  topRight: [y - 1, x + 1],
+  right: [y, x + 1],
+  rightBottom: [y + 1, x + 1],
+  bottom: [y + 1, x],
+  bottomLeft: [y + 1, x - 1],
+  left: [y, x - 1],
+  leftTop: [y - 1, x - 1],
+});
+
+export const incrementNeighbours = (coords: Coords, field: Field): Field => {
   return field;
 };
